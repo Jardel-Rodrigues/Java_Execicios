2f7c863394e92760fe13497ce0d63bce73a1add3
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2022-05-02 23:05:28 -0300

    Adicionando arquivo via upload

---
+++ Pedidos/src/entities/OrderItem.java
@@ -0,0 +1,66 @@
+package entities;
+
+public class OrderItem {
+	
+	private Integer quantity;
+	private Double price;
+	
+    private Product product;
+	
+	public OrderItem() {
+		
+	}
+		
+	public OrderItem(Integer quatity, Double price, Product product) {
+
+		this.quantity = quatity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuatity() {
+		return quantity;
+	}
+
+
+	public void setQuatity(Integer quatity) {
+		this.quantity = quatity;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+	public Double subTotal() {
+		return price * quantity;
+	}
+
+	@Override
+	public String toString() {
+		return getProduct().getName()
+		+ ", $"
+		+ String.format("%.2f", price)
+		+ " Quantity: "
+		+ quantity
+		+ " Subtotal: $"
+		+ String.format("%.2f ", subTotal());					
+	}
+	
+}
